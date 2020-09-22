8f9356b655c8081cc807a7f3081705b05767c4f1
Masterchef365 <duncan.freeman1@gmail.com>
2020-09-22 00:25:53 -0700

    Move rainbow cube into a function

--- examples/cube.rs
+++ examples/cube.rs
@@ -3,7 +3,7 @@ use klystron::{
     runtime::{launch, App},
     DrawType, Engine, FramePacket, Material, Mesh, Object, Vertex,
 };
-use nalgebra::Matrix4;
+use nalgebra::{Matrix4, Point3};
 use std::fs;
 
 struct MyApp {
@@ -22,46 +22,7 @@ impl App for MyApp {
             DrawType::Triangles,
         )?;
 
-        let vertices = [
-            Vertex {
-                pos: [-1.0, -1.0, -1.0],
-                color: [0.0, 1.0, 1.0],
-            },
-            Vertex {
-                pos: [1.0, -1.0, -1.0],
-                color: [1.0, 0.0, 1.0],
-            },
-            Vertex {
-                pos: [1.0, 1.0, -1.0],
-                color: [1.0, 1.0, 0.0],
-            },
-            Vertex {
-                pos: [-1.0, 1.0, -1.0],
-                color: [0.0, 1.0, 1.0],
-            },
-            Vertex {
-                pos: [-1.0, -1.0, 1.0],
-                color: [1.0, 0.0, 1.0],
-            },
-            Vertex {
-                pos: [1.0, -1.0, 1.0],
-                color: [1.0, 1.0, 0.0],
-            },
-            Vertex {
-                pos: [1.0, 1.0, 1.0],
-                color: [0.0, 1.0, 1.0],
-            },
-            Vertex {
-                pos: [-1.0, 1.0, 1.0],
-                color: [1.0, 0.0, 1.0],
-            },
-        ];
-
-        let indices = [
-            0, 1, 3, 3, 1, 2, 1, 5, 2, 2, 5, 6, 5, 4, 6, 6, 4, 7, 4, 0, 7, 7, 0, 3, 3, 2, 7, 7, 2,
-            6, 4, 5, 0, 0, 5, 1,
-        ];
-
+        let (vertices, indices) = rainbow_cube();
         let mesh = engine.add_mesh(&vertices, &indices)?;
 
         Ok(Self {
@@ -89,4 +50,24 @@ impl App for MyApp {
 fn main() -> Result<()> {
     env_logger::init();
     launch::<MyApp>()
+}
+
+fn rainbow_cube() -> (Vec<Vertex>, Vec<u16>) {
+    let vertices = vec![
+        Vertex::from_nalgebra(Point3::new(-1.0, -1.0, -1.0), Point3::new(0.0, 1.0, 1.0)),
+        Vertex::from_nalgebra(Point3::new(1.0, -1.0, -1.0), Point3::new(1.0, 0.0, 1.0)),
+        Vertex::from_nalgebra(Point3::new(1.0, 1.0, -1.0), Point3::new(1.0, 1.0, 0.0)),
+        Vertex::from_nalgebra(Point3::new(-1.0, 1.0, -1.0), Point3::new(0.0, 1.0, 1.0)),
+        Vertex::from_nalgebra(Point3::new(-1.0, -1.0, 1.0), Point3::new(1.0, 0.0, 1.0)),
+        Vertex::from_nalgebra(Point3::new(1.0, -1.0, 1.0), Point3::new(1.0, 1.0, 0.0)),
+        Vertex::from_nalgebra(Point3::new(1.0, 1.0, 1.0), Point3::new(0.0, 1.0, 1.0)),
+        Vertex::from_nalgebra(Point3::new(-1.0, 1.0, 1.0), Point3::new(1.0, 0.0, 1.0)),
+    ];
+
+    let indices = vec![
+        0, 1, 3, 3, 1, 2, 1, 5, 2, 2, 5, 6, 5, 4, 6, 6, 4, 7, 4, 0, 7, 7, 0, 3, 3, 2, 7, 7, 2, 6,
+        4, 5, 0, 0, 5, 1,
+    ];
+
+    (vertices, indices)
 }
